f814e44a9cf0074c599f1e3f1a4a8adc1b690571
Aaksh Ranjan <aakshranjan@outlook.com>
2024-02-28 22:02:38 -0800

    Removing the run.py file which was used for testing.

--- package/advanced-logging/src/general/run.py
+++
@@ -1,8 +0,0 @@
-from general_logging import GeneralLogging
-
-# Create a logger
-logger = GeneralLogging()
-
-# Get the logger name
-logger_name = logger.logger.name
-print("Logger Name:", logger_name)
